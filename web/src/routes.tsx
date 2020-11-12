1523d4aca87191070ef1f5a165549ca9be815a1b
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-12 20:12:34 -0300

    routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,1 +1,14 @@
-import {} from ''
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing'
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path="/" component={Landing} />
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
