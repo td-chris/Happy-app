84bf6d12e7bae9f4bf51d93901d800201826b50c
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-16 15:18:25 -0300

    express comments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,29 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+// Comentarios Express
+
+// Rota = conjunto da obra
+// Recurso = usuario
+
+// Métodos HTTP = GET, POST, PUT , DELETE
+
+// GET = Buscar informação (lista, item)
+// POST = Criando uma informação
+// PUT = Editando uma infromação
+// DELETE = Deletando uma infromação
+
+// Parámetros
+
+// Query Params: http//localhost:3333/users?search=christian
+// Route Params: http//localhost:3333/users/1 (Identificar um recurso)
+// Body: http//localhost:3333/users/1 (parametros vindos de um formulario)
+
+app.get("/users/:id", (request, response) => {
+    return response.json({ message: 'Hello World' });
+});
+
 app.listen(3333);
 //LOCALHOST:3333
