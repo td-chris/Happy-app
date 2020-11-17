cbb2b2c381c0ad4cd94417bd40ad7beed60f00e0
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-17 17:28:34 -0300

    create views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphananges_view';
+
 import Orphanage from '../models/Orphanage';
 
 //MVC
@@ -15,9 +17,11 @@ export default {
     async index(request: Request, response: Response){
         const orphanagesRepository = getRepository(Orphanage);
         
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
     
-        return response.json(orphanages);
+        return response.json(orphanageView.renderMany(orphanages));
     },
 
     async show(request: Request, response: Response){
@@ -25,9 +29,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
         
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
     
-        return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
 
     async create(request: Request, response: Response) {
