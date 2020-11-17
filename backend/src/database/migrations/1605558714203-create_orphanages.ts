ad27f0a56eb998e60c225b83cd89dadd4194b719
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-17 13:29:42 -0300

    create Models and add opening hours on migration

--- backend/src/database/migrations/1605558714203-create_orphanages.ts
+++ backend/src/database/migrations/1605558714203-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1605558714203 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
