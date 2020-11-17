28917153cf51603010c6e7b58f87ac86910d541e
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-17 13:59:20 -0300

    configuring entities and orphanage creation on db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 // REQ / RES
 
 import './database/connection';
@@ -25,9 +27,31 @@ app.use(express.json());
 // Route Params: http//localhost:3333/users/1 (Identificar um recurso)
 // Body: http//localhost:3333/users/1 (parametros vindos de um formulario)
 
-app.post("/orphanages", (request, response) => {
+app.post("/orphanages", async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
     
-    
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
     return response.json({ message: 'Hello World' });
 });
 
