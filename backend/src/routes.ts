361fb21722869e60ea99f688efb2baaed0b84fed
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-17 17:03:14 -0300

    upload created and added images on db

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 // Comentarios Express
 
@@ -23,6 +27,6 @@ const routes = Router();
 
 routes.get("/orphanages", OrphanagesController.index);
 routes.get("/orphanages/:id", OrphanagesController.show);
-routes.post("/orphanages", OrphanagesController.create);
+routes.post("/orphanages",upload.array('images') , OrphanagesController.create);
 
 export default routes;
