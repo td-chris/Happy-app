bbcc341607b85c14afa55bcae1bb7a9c6deeabf2
Christian Todesquine Dias <todesquinechristian@gmail.com>
2020-11-18 12:19:53 -0300

    create Sidebar component

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
+import { FiClock, FiInfo } from "react-icons/fi";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
 import L from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import Sidebar from "../components/Sidebar";
 
 import '../styles/pages/orphanage.css';
+
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -18,19 +19,12 @@ const happyMapIcon = L.icon({
 })
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
+
 
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      
+      <Sidebar />
 
       <main>
         <div className="orphanage-details">
